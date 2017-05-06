a692acb3d34c83453069e88a287a3912a886b176
ajwehunt <adamjwehunt@gmail.com>
2017-05-05 23:11:16 -0700

    more html/css

--- public/js/controllers/mainCtrl.js
+++ public/js/controllers/mainCtrl.js
@@ -22,4 +22,25 @@ angular.module('strongtower')
       navText: ['<div class="btn-circle"><i class="fa fa-chevron-left" aria-hidden="true"></i></div>','<div class="btn-circle"><i class="fa fa-chevron-right" aria-hidden="true"></i></div>']
   };
 
+  $scope.owlpropertiesPartners = {
+      items: 3,
+      autoplay: true,
+      autoplayTimeout: 5000,
+      autoplaySpeed: 100,
+      dots: true,
+      loop: true,
+      nav: true,
+      margin: 10,
+      slideBy: 'page',
+      responsive: {
+        768: {
+          items: 4
+        },
+        1200: {
+          items: 6
+        }
+      },
+      navText: ['<i class="fa fa-chevron-left" aria-hidden="true"></i>','<i class="fa fa-chevron-right" aria-hidden="true"></i>']
+  };
+
 })
