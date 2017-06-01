9f9682382c3886b1cf50daf8881e15312ab4d69f
ajwehunt <adamjwehunt@gmail.com>
2017-05-31 21:38:56 -0700

    added social media feed

--- public/dist/all.js
+++ public/dist/all.js
@@ -93,3 +93,10 @@ angular.module('strongtower').controller('mainCtrl', function ($scope, $timeout,
     navText: ['<i class="fa fa-chevron-left" aria-hidden="true"></i>', '<i class="fa fa-chevron-right" aria-hidden="true"></i>']
   };
 });
+// (function(d, s, id) {
+//   var js, fjs = d.getElementsByTagName(s)[0];
+//   if (d.getElementById(id)) return;
+//   js = d.createElement(s); js.id = id;
+//   js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.9&appId=902009893274771";
+//   fjs.parentNode.insertBefore(js, fjs);
+// }(document, 'script', 'facebook-jssdk'))
--- public/js/controllers/mainCtrl.js
+++ public/js/controllers/mainCtrl.js
@@ -49,3 +49,10 @@ angular.module('strongtower')
   };
 
 })
+// (function(d, s, id) {
+//   var js, fjs = d.getElementsByTagName(s)[0];
+//   if (d.getElementById(id)) return;
+//   js = d.createElement(s); js.id = id;
+//   js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.9&appId=902009893274771";
+//   fjs.parentNode.insertBefore(js, fjs);
+// }(document, 'script', 'facebook-jssdk'))
